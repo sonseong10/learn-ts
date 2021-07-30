adc076270d22e3404312f6d1c507dbd0677b91ff
sonseong10 <sonseong10@gmail.com>
2021-07-30 15:43:37 +0900

    Learn: use like TS in JS

---
+++ javascript/7-4-js-to-ts.js
@@ -0,0 +1,18 @@
+{
+  function plus(a, b) {
+    return a + b
+  }
+
+  plus(3, 6)
+
+  /**
+   *
+   * @param {number} a 첫번째 숫자
+   * @param {number} b 두번째 숫자
+   */
+  function sum(a, b) {
+    return a + b
+  }
+  // sum(3, "💩")
+  sum(3, 6)
+}
